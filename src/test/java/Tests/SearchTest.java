9e3a0deea7349d951c7db5c14dace5d5cb0fa37e
DiDa <Monam2715@gmail.com>
2024-09-27 17:08:55 +0300

    commit 1 commit page base commit sign in page commit Search page commit deals page commit test base commit test util commit signin test commit search test

---
+++ src/test/java/Tests/SearchTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchTest extends TestBase{
+    @Test
+    public void Scenario1() {
+        searchPage.enterSearchValue("car accessories");
+        searchPage.clickOnSearchButton();
+        Assert.assertEquals(driver.getTitle(),"Amazon.com : car accessories");
+        // searchPage.dismissToaster();
+        searchPage.addToCart();
+        //searchPage.clickOnCartIcon();
+        //Assert.assertEquals(driver.getTitle(),"Amazon.com Shopping Cart");
+        //Assert.assertTrue(searchPage.checkTotal());
+    }
+}
