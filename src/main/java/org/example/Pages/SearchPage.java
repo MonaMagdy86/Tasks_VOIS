d98becb6aa3b4ffc834b476612800e5299affbae
DiDa <Monam2715@gmail.com>
2024-09-27 19:09:30 +0300

    commit 1 commit page base commit sign in page commit Search page commit deals page commit test base commit test util commit signin test commit search test commit deals test commit updates commit select item commit scenario 1 commit search page refactor

--- src/main/java/org/example/Pages/SearchPage.java
+++ src/main/java/org/example/Pages/SearchPage.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class SearchPage extends PageBase {
     By searchBox = By.id("twotabsearchtextbox");
     By searchButton = By.id("nav-search-submit-button");
-    By addToCartButton=By.xpath("//button[@class=\"puis-atcb-container\"]");
     By cartIcon = By.id("nav-cart-count");
     By checkSubtotal=By.id("sc-subtotal-label-buybox");
     By toaster=By.xpath("//input[@data-action-type=\"DISMISS\"]");
@@ -29,40 +28,15 @@ public class SearchPage extends PageBase {
     public void clickOnSearchButton() {
         driver.findElement(searchButton).click();
     }
-    public void dismissToaster(){
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(toaster))).click();
-    }
-    public void addToCart(){
-        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(30))      // Total timeout
-                .pollingEvery(Duration.ofSeconds(5))      // Polling interval
-                .ignoring(NoSuchElementException.class);  // Ignore specific exceptions
-
-        WebElement element = fluentWait.until(driver -> driver.findElement(addToCartButton));
-        //WebElement element = driver.findElement(addToCartButton);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        List<WebElement> elements =driver.findElements(addToCartButton);
-        WebElement firstElement = elements.get(0);
-        System.out.println(firstElement.getText());
-        firstElement.click();
-    /*WebElement element = driver.findElement(addToCartButton);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(addToCartButton)));
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(addToCartButton))).click();
-   */
-    }
     public void selectFirstItem(){
         List<WebElement> elements =driver.findElements(selectItem);
         WebElement firstElement = elements.get(0);
-        //System.out.println(firstElement.getText());
         firstElement.click();
     }
-
     public void addItemToCart(){
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(addItem)));
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(addItem))).click();
     }
-
     public void clickOnCartIcon(){
         driver.findElement(cartIcon).click();
     }
