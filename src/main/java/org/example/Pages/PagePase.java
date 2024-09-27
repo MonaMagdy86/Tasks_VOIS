40987618f8fc369749dde0cd6fd15af0fd257eb0
DiDa <Monam2715@gmail.com>
2024-09-27 16:53:04 +0300

    commit 1 commit page base

---
+++ src/main/java/org/example/Pages/PagePase.java
@@ -0,0 +1,17 @@
+package org.example.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PagePase {
+    protected static WebDriver driver;
+    WebDriverWait wait;
+
+    public PageBase(WebDriver driver){
+
+        this.driver =driver;
+        wait = new WebDriverWait(this.driver, Duration.ofSeconds(100));
+    }
+}
